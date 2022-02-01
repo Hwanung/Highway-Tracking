3a24db512994091ec08fcb57e384f71b3ce9397c
Hwanung <71459504+Hwanung@users.noreply.github.com>
2022-02-01 23:40:09 +0100

    works well but needs a lot of improvements
    can track vehicles and assign a number to each of them. tracker.py was not done by myself so i need to do it.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
+from tracker import *
+import numpy as np
 
-
+tracker = EuclideanDistTracker()
 capture = cv2.VideoCapture("highway.mp4") #video path
 delay = 30
 
@@ -19,12 +21,22 @@ while 1 : #each loop = one frame
 
     #obj detec
     mask = object_dec.apply(region)
+    _, mask = cv2.threshold(mask, 254 ,255 ,cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for x in contours:
+    det = []
+    for c in contours:
         #calculate area and remove small elements :
-        area = cv2.contourArea(x)
+        area = cv2.contourArea(c)
         if area > 100:
-            cv2.drawContours(region, [x], -1, (0, 255, 0), 2)
+            #cv2.drawContours(region, [c], -1, (0, 255, 0), 2)
+            x,y,w,h = cv2.boundingRect(c)
+            det.append([x,y,w,h])
+    ids = tracker.update(det)
+    for i in ids :
+        x, y, w, h, id = i
+        cv2.putText(region,str(id),(x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(region, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
     cv2.imshow("region",region)
     cv2.imshow("Mask", mask) #detected objects are white
     cv2.imshow("Frame", frame)
